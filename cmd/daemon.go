12a75a4ac243927628bcd3fcf3b5f6c5ab35b05c
Ivan Schasny <ivan.schasny@protocol.ai>
2022-09-19 11:37:58 +0100

    Added daemon and main

--- cmd/daemon.go
+++ cmd/daemon.go
@@ -1,11 +1,17 @@
-package main
+package command
 
 import (
-	logging "github.com/ipfs/go-log/v2"
+	"context"
+	"strings"
+
 	"github.com/urfave/cli/v2"
+
+	"github.com/ipfs-shipyard/ipfs-index-provider/server"
+	"github.com/ipfs/go-datastore"
+	logging "github.com/ipfs/go-log/v2"
 )
 
-var log = logging.Logger("command/reference-provider")
+var log = logging.Logger("cmd/daemon")
 
 var daemonFlags = []cli.Flag{
 	&cli.StringFlag{
@@ -15,11 +21,16 @@ var daemonFlags = []cli.Flag{
 		Value:    "info",
 		Required: false,
 	},
+	&cli.StringFlag{
+		Name:     "direct-announce",
+		Usage:    "List of http urls to direct announce ads to",
+		Required: true,
+	},
 }
 
 var DaemonCmd = &cli.Command{
 	Name:   "daemon",
-	Usage:  "Starts a reference provider",
+	Usage:  "Starts ipfs-index-provider",
 	Flags:  daemonFlags,
 	Action: daemonCommand,
 }
@@ -30,5 +41,121 @@ func daemonCommand(cctx *cli.Context) error {
 		return err
 	}
 
+	urls := strings.Split(cctx.String("direct-announce"), ",")
+
+	s, err := server.NewServer(datastore.NewMapDatastore(), server.WithCidsPerChunk(1), server.WithDirectAnnounceUrls(urls))
+	if err != nil {
+		log.Info("Error creating server %s\n", err)
+		return err
+	}
+
+	log.Info("Starting server")
+	err = s.Start(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		log.Info("Shutting down")
+		s.Shutdown(context.Background())
+	}()
+
 	return nil
 }
+
+// func createServer() (*server.Server, error) {
+// 	// --- config
+
+// 	// cfg, err := config.Load("")
+// 	// if err != nil {
+// 	// 	if err == config.ErrNotInitialized {
+// 	// 		return nil, errors.New("reference provider is not initialized\nTo initialize, run using the \"init\" command")
+// 	// 	}
+// 	// 	return nil, fmt.Errorf("cannot load config file: %w", err)
+// 	// }
+
+// 	// --- libp2p
+
+// 	// _, privKey, err := cfg.Identity.Decode()
+// 	// if err != nil {
+// 	// 	return nil, err
+// 	// }
+
+// 	// p2pmaddr, err := multiaddr.NewMultiaddr(cfg.ReframeServer.ListenMultiaddr)
+// 	// if err != nil {
+// 	// 	return nil, fmt.Errorf("bad p2p address in config %s: %s", cfg.ReframeServer.ListenMultiaddr, err)
+// 	// }
+
+// 	// h, err := libp2p.New(
+// 	// 	// Use the keypair generated during init
+// 	// 	libp2p.Identity(privKey),
+// 	// 	// Listen to p2p addr specified in config
+// 	// 	libp2p.ListenAddrs(p2pmaddr),
+// 	// )
+
+// 	// Create a new libp2p host
+
+// 	// Starting provider core
+
+// 	// ---   datastore
+
+// 	// dataStorePath, err := config.Path("", "datastore")
+// 	// if err != nil {
+// 	// 	return nil, err
+// 	// }
+// 	// err = checkWritable(dataStorePath)
+// 	// if err != nil {
+// 	// 	return nil, err
+// 	// }
+// 	// ds, err := leveldb.NewDatastore(dataStorePath, nil)
+// 	// if err != nil {
+// 	// 	return nil, err
+// 	// }
+
+// 	/// --- server
+
+// 	s, err := server.NewServer(datastore.NewMapDatastore(), server.WithCidsPerChunk(1), server.WithDirectAnnounceUrls([]string{"http://127.0.0.1:3001"}))
+// 	if err != nil {
+// 		log.Info("Error initialising server %s\n", err)
+// 		return nil, err
+// 	}
+
+// 	return s, nil
+// }
+
+// checkWritable checks the the directory is writable.
+// If the directory does not exist it is created with writable permission.
+// func checkWritable(dir string) error {
+// 	if dir == "" {
+// 		return errors.New("cannot check empty directory")
+// 	}
+
+// 	var err error
+// 	dir, err = homedir.Expand(dir)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	if _, err = os.Stat(dir); err != nil {
+// 		switch {
+// 		case errors.Is(err, os.ErrNotExist):
+// 			// dir doesn't exist, check that we can create it
+// 			return os.Mkdir(dir, 0o775)
+// 		case errors.Is(err, os.ErrPermission):
+// 			return fmt.Errorf("cannot write to %s, incorrect permissions", err)
+// 		default:
+// 			return err
+// 		}
+// 	}
+
+// 	// dir exists, make sure we can write to it
+// 	testfile := filepath.Join(dir, "test")
+// 	fi, err := os.Create(testfile)
+// 	if err != nil {
+// 		if os.IsPermission(err) {
+// 			return fmt.Errorf("%s is not writeable by the current user", dir)
+// 		}
+// 		return fmt.Errorf("unexpected error while checking writeablility of repo root: %s", err)
+// 	}
+// 	fi.Close()
+// 	return os.Remove(testfile)
+// }
--- main.go
+++ main.go
@@ -2,148 +2,45 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"os"
-	"path/filepath"
+	"os/signal"
+	"syscall"
 
-	"github.com/ipfs-shipyard/ipfs-index-provider/server"
-	"github.com/ipfs/go-datastore"
-	logging "github.com/ipfs/go-log/v2"
-	"github.com/mitchellh/go-homedir"
+	command "github.com/ipfs-shipyard/ipfs-index-provider/cmd"
+	"github.com/urfave/cli/v2"
 )
 
-var log = logging.Logger("main")
-
 func main() {
-	setLoggingConfig()
-	s, err := createServer()
-	if err != nil {
-		panic(err)
-	}
+	// Set up a context that is canceled when the command is interrupted
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	log.Info("Starting server")
-	err = s.Start(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		log.Info("Shutting down")
-		s.Shutdown(context.Background())
+	// Set up a signal handler to cancel the context
+	go func() {
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, syscall.SIGTERM, syscall.SIGINT)
+		select {
+		case <-interrupt:
+			cancel()
+			fmt.Println("Received interrupt signal, shutting down...")
+			fmt.Println("(Hit ctrl-c again to force-shutdown the daemon.)")
+		case <-ctx.Done():
+		}
+		// Allow any further SIGTERM or SIGINT to kill process
+		signal.Stop(interrupt)
 	}()
-}
 
-func createServer() (*server.Server, error) {
-	// --- config
-
-	// cfg, err := config.Load("")
-	// if err != nil {
-	// 	if err == config.ErrNotInitialized {
-	// 		return nil, errors.New("reference provider is not initialized\nTo initialize, run using the \"init\" command")
-	// 	}
-	// 	return nil, fmt.Errorf("cannot load config file: %w", err)
-	// }
-
-	// --- libp2p
-
-	// _, privKey, err := cfg.Identity.Decode()
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// p2pmaddr, err := multiaddr.NewMultiaddr(cfg.ReframeServer.ListenMultiaddr)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("bad p2p address in config %s: %s", cfg.ReframeServer.ListenMultiaddr, err)
-	// }
-
-	// h, err := libp2p.New(
-	// 	// Use the keypair generated during init
-	// 	libp2p.Identity(privKey),
-	// 	// Listen to p2p addr specified in config
-	// 	libp2p.ListenAddrs(p2pmaddr),
-	// )
-
-	// Create a new libp2p host
-
-	// Starting provider core
-
-	// ---   datastore
-
-	// dataStorePath, err := config.Path("", "datastore")
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// err = checkWritable(dataStorePath)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// ds, err := leveldb.NewDatastore(dataStorePath, nil)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	/// --- server
-
-	s, err := server.NewServer(datastore.NewMapDatastore(), server.WithCidsPerChunk(1), server.WithDirectAnnounceUrls([]string{"http://127.0.0.1:3001"}))
-	if err != nil {
-		log.Info("Error initialising server %s\n", err)
-		return nil, err
+	app := &cli.App{
+		Name:  "ipfs-index-provider",
+		Usage: "IPFS Index Provider Node: bridge between IPFS and indexer",
+		Commands: []*cli.Command{
+			command.DaemonCmd,
+		},
 	}
 
-	return s, nil
-}
-
-// checkWritable checks the the directory is writable.
-// If the directory does not exist it is created with writable permission.
-func checkWritable(dir string) error {
-	if dir == "" {
-		return errors.New("cannot check empty directory")
+	if err := app.RunContext(ctx, os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-
-	var err error
-	dir, err = homedir.Expand(dir)
-	if err != nil {
-		return err
-	}
-
-	if _, err = os.Stat(dir); err != nil {
-		switch {
-		case errors.Is(err, os.ErrNotExist):
-			// dir doesn't exist, check that we can create it
-			return os.Mkdir(dir, 0o775)
-		case errors.Is(err, os.ErrPermission):
-			return fmt.Errorf("cannot write to %s, incorrect permissions", err)
-		default:
-			return err
-		}
-	}
-
-	// dir exists, make sure we can write to it
-	testfile := filepath.Join(dir, "test")
-	fi, err := os.Create(testfile)
-	if err != nil {
-		if os.IsPermission(err) {
-			return fmt.Errorf("%s is not writeable by the current user", dir)
-		}
-		return fmt.Errorf("unexpected error while checking writeablility of repo root: %s", err)
-	}
-	fi.Close()
-	return os.Remove(testfile)
-}
-
-func setLoggingConfig() error {
-	// Set overall log level.
-	err := logging.SetLogLevel("*", "info")
-	if err != nil {
-		return err
-	}
-
-	// Set level for individual loggers.
-	// for loggerName, level := range cfgLogging.Loggers {
-	// 	err = logging.SetLogLevel(loggerName, level)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
-	return nil
 }
